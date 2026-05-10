b85269cdf085176609650d1a3eae8bbd5a9c2ea6
pedro <pdrolucas.dev@gmail.com>
2026-05-09 21:10:49 -0300

    feat: first commit

---
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Filament\Resources\Posts\Schemas;
+
+use App\Models\Category;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\ColorPicker;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TagsInput;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Group;
+use Filament\Schemas\Components\Section;
+use Filament\Schemas\Schema;
+
+class PostForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Section::make("Basic Info")
+                    ->description("Add a basic info for this post here")
+                    ->schema([
+                        Group::make()
+                            ->schema([
+                                TextInput::make("title"),
+                                TextInput::make("slug"),
+                                Select::make("category_id")
+                                    ->label("Category")
+                                    ->options(Category::all()->pluck("name", "id")),
+                                ColorPicker::make("color"),
+                            ])
+                            ->columns(2),
+                        MarkdownEditor::make("body"),
+                    ])
+                    ->columnSpan(2),
+
+                Group::make()->schema([
+                    Section::make("Image Cover")
+                        ->description("Upload an image cover for this post")
+                        ->schema([FileUpload::make("image")->disk("public")->directory("posts")]),
+
+                    Section::make("Metadata")
+                        ->description("Add metadata for this post")
+                        ->schema([
+                            TagsInput::make("tags"),
+                            Checkbox::make("published"),
+                            DatePicker::make("published_at"),
+                        ]),
+                ]),
+            ])
+            ->columns(3);
+    }
+}
---
+++ app/Filament/Resources/Posts/Tables/PostsTable.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Filament\Resources\Posts\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Columns\ColorColumn;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class PostsTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                ImageColumn::make("image")->disk("public")->circular()->size(32),
+                TextColumn::make("title"),
+                TextColumn::make("slug"),
+                TextColumn::make("category.name"),
+                ColorColumn::make("color"),
+                TextColumn::make("published_at"),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([EditAction::make()])
+            ->toolbarActions([BulkActionGroup::make([DeleteBulkAction::make()])]);
+    }
+}
